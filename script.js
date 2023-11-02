67f80be41b7ad91122d6357981725fa5f7640d73
Andrew <andrewozo123@gmail.com>
2023-11-02 11:10:16 -0400

    set up script and added listeners

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // Write your code here
+let lightBulb1 = document.querySelector("#lightbulb1");
+let lightBulb2 = document.querySelector("#lightbulb2");
+let lightBulb3 = document.querySelector("#lightbulb3");
+let subtitle = document.querySelector(".subtitle");
+let count = 0;
+
+lightBulb1.addEventListener("click", () => {
+  count++;
+  subtitle.innerHTML = `You've clicked the lights ${count} times`;
+  lightBulb1.classList.toggle("active");
+});
+lightBulb2.addEventListener("click", () => {
+  count++;
+  subtitle.innerHTML = `You've clicked the lights ${count} times`;
+
+  lightBulb2.classList.toggle("active");
+});
+lightBulb3.addEventListener("click", () => {
+  count++;
+  subtitle.innerHTML = `You've clicked the lights ${count} times`;
+  lightBulb3.classList.toggle("active");
+});
